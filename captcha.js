7435fb76a2df1dcdd755ba4d318d8e6b1bc42dce
Leo Developer <leodeveloper@protonmail.com>
2022-08-17 14:52:06 +0200

    fix rare bug

--- captcha.js
+++ captcha.js
@@ -4,12 +4,12 @@ if(window.location.hash.startsWith("#sage/")) {
     css.innerText = `.section_title,.row_flex,.form_row:last-child,.responsive_header,#global_header,#footer,#footer_spacer{display:none !important;}#captcha_entry{position: fixed;top: 0;left: 0;height: 100vh;width: 100vw;display: flex;justify-content: center;align-items: center;}`;
     document.head.appendChild(css);
     setInterval(() => {
-        const token = document.getElementById("g-recaptcha-response").value;
-        if(token) {
+        const token = document.getElementById("g-recaptcha-response");
+        if(token && token.value) {
             const gid = document.getElementById("captchagid").value;
-            console.log("[SAGE] captcha solved", token);
+            console.log("[SAGE] captcha solved", token.value);
             const session = window.location.hash.split("/")[1];
-            document.location = `https://sage.leodev.xyz/gen#token=${token}&gid=${gid}&session=${session}`;
+            document.location = `https://sage.leodev.xyz/gen#token=${token.value}&gid=${gid}&session=${session}`;
         }
     }, 500);
 }
