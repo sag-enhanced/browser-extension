b64ae81bdf0dca971198c441e46691947049e1d5
Leo Developer <leodeveloper@protonmail.com>
2022-08-26 17:13:22 +0200

    hide more stuff using css

--- captcha.js
+++ captcha.js
@@ -1,7 +1,7 @@
 if(window.location.hash.startsWith("#sage/")) {
     console.log("[SAGE] init");
     const css = document.createElement("style");
-    css.innerText = `.section_title,.row_flex,.form_row:last-child,.responsive_header,#global_header,#footer,#footer_spacer{display:none !important;}#captcha_entry{position: fixed;top: 0;left: 0;height: 100vh;width: 100vw;display: flex;justify-content: center;align-items: center;}`;
+    css.innerText = `.section_title,.row_flex,.form_row:last-child,.responsive_header,#global_header,#footer,#footer_spacer{display:none !important;}.responsive_page_content,.responsive_page_template_content,.joinsteam_content_container{padding:0px !important;}#captcha_entry{position: fixed;top: 0;left: 0;height: 100vh;width: 100vw;display: flex;justify-content: center;align-items: center;}`;
     document.head.appendChild(css);
     setInterval(() => {
         const token = document.getElementById("g-recaptcha-response");
