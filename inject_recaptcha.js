fc3e25fb13ab99959337c0b60d41f8118df2e37a
Leo Developer <leodeveloper@protonmail.com>
2022-10-04 23:00:03 +0200

    various small race condition bug fixes and only consider localhost to be a valid dynamic origin

--- inject_recaptcha.js
+++ inject_recaptcha.js
@@ -1,9 +1,11 @@
 if(window.location.search.includes("6LdIFr0ZAAAAAO3vz0O0OQrtAefzdJcWQM2TMYQH")) {
     console.log("[SAGE] [RECAPTCHA] init");
-    setInterval(() => {
+    let intervalId;
+    intervalId = setInterval(() => {
         if(document.querySelector(".recaptcha-checkbox-expired")) {
             console.log("[SAGE] [RECAPTCHA] detected stuck recaptcha; reloading");
             window.location.reload();
+            clearInterval(intervalId);
         }
     }, 500);
 }
--- inject_steam.js
+++ inject_steam.js
@@ -6,13 +6,20 @@ if(window.location.hash === "#sage") {
         css.innerText = `.section_title,.row_flex,.form_row:last-child,.responsive_header,#global_header,#footer,#footer_spacer{display:none !important;}.responsive_page_content,.responsive_page_template_content,.joinsteam_content_container{padding:0px !important;}#captcha_entry{position: fixed;top: 0;left: 0;height: 100vh;width: 100vw;display: flex;justify-content: center;align-items: center;}`;
         document.head.appendChild(css);
     }, 0);
-    setInterval(() => {
+    let intervalId;
+    intervalId = setInterval(() => {
         const token = document.getElementById("g-recaptcha-response");
         if(token && token.value) {
             const gid = document.getElementById("captchagid").value;
             console.log("[SAGE] [STEAM] captcha solved", token.value);
-            const origin = document.referrer ? new URL(document.referrer).origin : "https://sage.leodev.xyz";
+            let origin = "https://sage.leodev.xyz";
+            if(document.referrer) {
+                const referrer = new URL(document.referrer);
+                if(referrer.origin && referrer.origin.includes("localhost"))
+                    origin = referrer.origin;
+            }
             document.location = `${origin}/gen#token=${token.value}&gid=${gid}`;
+            clearInterval(intervalId);
         };
     }, 500);
 }
